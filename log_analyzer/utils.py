47233ba26069b0c30034b16810cfc6e0ed42ed0b
Maxim Pokrovsky <maxim.pokrovsky@mp.com>
2025-04-07 10:24:49 +0300

    Exclude duplicates from entered parameters.

--- log_analyzer/utils.py
+++ log_analyzer/utils.py
@@ -39,14 +39,18 @@ def validate_file_paths_or_raise(file_paths: Sequence[str]) -> Sequence[str]:
     Otherwise throw an exception: FileNotFoundError.
     """
     not_exists_files = set()
+    exists_files = set()
 
     for file_path in file_paths:
         if not path.isfile(file_path):
             not_exists_files.add(file_path)
+            continue
+
+        exists_files.add(file_path)
 
     if not_exists_files:
         raise FileNotFoundError(
             "No files found: {}".format(", ".join(sorted(not_exists_files)))
         )
 
-    return file_paths
+    return sorted(exists_files)
