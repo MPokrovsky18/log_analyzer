b6ae4b1b68d927b43fdee4d506583c231fcb594a
Maxim Pokrovsky <maxim.pokrovsky@mp.com>
2025-04-07 12:41:41 +0300

    Init the function to format the 'handlers' report data and include it in the report register.

---
+++ log_analyzer/formatters.py
@@ -0,0 +1,5 @@
+def format_handlers_report(data: dict) -> str:
+    """
+    Formats data for the 'handlers' report into text.
+    """
+    pass
--- log_analyzer/reports_registry.py
+++ log_analyzer/reports_registry.py
@@ -1,8 +1,27 @@
+from typing import Callable, NamedTuple
+
+from log_analyzer.formatters import format_handlers_report
 from log_analyzer.report_collectors import (
     BaseReportCollector,
     HandlersReportCollector,
 )
 
-REPORTS: dict[str, BaseReportCollector] = {
-    "handlers": HandlersReportCollector,
+
+class Report(NamedTuple):
+    """
+    A named tuple containing a collector and formatter pair for reporting.
+    """
+
+    report_collector: BaseReportCollector
+    formatter: Callable[[dict], str]
+
+
+HANDLERS_REPORT = Report(
+    report_collector=HandlersReportCollector,
+    formatter=format_handlers_report
+)
+
+
+REPORTS: dict[str, Report] = {
+    "handlers": HANDLERS_REPORT,
 }
