c84a002832a5ddec3823713da89117a0a3406af2
Maxim Pokrovsky <maxim.pokrovsky@mp.com>
2025-04-07 18:31:11 +0300

    Write an implementation of methods for collecting data from a file and aggregating it for subsequent formatting.

--- log_analyzer/report_collectors.py
+++ log_analyzer/report_collectors.py
@@ -46,33 +46,73 @@ class HandlersReportCollector(BaseReportCollector):
     """
 
     def _process_file(self, path: str) -> dict:
-        # data after file processing.
-        return {"data": "data"}
+        data = {}
+
+        with open(path, "r") as file:
+            for line in file:
+                handler_info = self._parse_line(line)
+
+                if not handler_info:
+                    continue
+
+                handler = handler_info["handler"]
+                log_level = handler_info["log_level"]
+
+                if handler not in data:
+                    data[handler] = {}
+
+                if log_level not in data[handler]:
+                    data[handler][log_level] = 0
+
+                data[handler][log_level] += 1
+
+        return data
+
+    def _parse_line(self, line: str) -> dict | None:
+        if line.find("django.request") < 0:
+            return
+
+        data = line.split()
+        handler = list(filter(lambda x: x.startswith("/"), data))[0]
+
+        log_levels = {"DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"}
+
+        log_level = list(filter(lambda x: x in log_levels, data))[0]
+
+        return {
+            "handler": handler,
+            "log_level": log_level,
+        }
 
     def _aggregate_data(self, data: list[dict]) -> dict:
+        total_requests = 0
+        handlers = {}
+        total_handlers_stats = {
+            "DEBUG": 0,
+            "INFO": 0,
+            "WARNING": 0,
+            "ERROR": 0,
+            "CRITICAL": 0,
+        }
+
+        for file_data in data:
+            for handler, logs_info in file_data.items():
+                if handler not in handlers:
+                    handlers[handler] = {
+                        "DEBUG": 0,
+                        "INFO": 0,
+                        "WARNING": 0,
+                        "ERROR": 0,
+                        "CRITICAL": 0,
+                    }
+
+                for log_level, count in logs_info.items():
+                    handlers[handler][log_level] += count
+                    total_handlers_stats[log_level] += count
+                    total_requests += count
+
         return {
-            "total requests": 1000,
-            "handlers": {
-                "handler1": {
-                    "DEBUG": 100,
-                    "INFO": 100,
-                    "WARNING": 100,
-                    "ERROR": 100,
-                    "CRITICAL": 100,
-                },
-                "handler2": {
-                    "DEBUG": 100,
-                    "INFO": 100,
-                    "WARNING": 100,
-                    "ERROR": 100,
-                    "CRITICAL": 100,
-                },
-            },
-            "total handlers stats": {
-                "DEBUG": 200,
-                "INFO": 200,
-                "WARNING": 200,
-                "ERROR": 200,
-                "CRITICAL": 200,
-            }
+            "total requests": total_requests,
+            "handlers": handlers,
+            "total handlers stats": total_handlers_stats,
         }
