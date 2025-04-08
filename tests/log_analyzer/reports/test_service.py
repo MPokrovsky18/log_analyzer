45f456bfabe97da6f5fa4a1683390f7d8dbf52ea
Maxim Pokrovsky <maxim.pokrovsky@mp.com>
2025-04-08 21:20:27 +0300

    Update tests for factory and service modules.

--- tests/log_analyzer/reports/test_service.py
+++ tests/log_analyzer/reports/test_service.py
@@ -4,14 +4,25 @@ from log_analyzer.reports.exceptions import ReportNameError
 from log_analyzer.reports.factory import ReportServiceCreator
 from log_analyzer.reports.service import ReportService
 
+from tests.constants import EXISTS_REPORT_NAME, NOT_EXISTS_REPORT_NAME
+
 
 def test_report_service_creator_creates_valid_service():
-    service = ReportServiceCreator.create("handlers")
+    """
+    Test that a ReportService is successfully created
+    by a valid report name.
+    """
+    service = ReportServiceCreator.create(EXISTS_REPORT_NAME)
+
     assert isinstance(service, ReportService)
     assert hasattr(service, "_collector")
     assert hasattr(service, "_formatter")
 
 
 def test_report_service_creator_raises_on_invalid_name():
+    """
+    Test that creating ReportService with an invalid name
+    raises a ReportNameError.
+    """
     with pytest.raises(ReportNameError):
-        ReportServiceCreator.create("non_existing_report")
+        ReportServiceCreator.create(NOT_EXISTS_REPORT_NAME)
